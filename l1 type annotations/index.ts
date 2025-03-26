3686022c0ea346b34da3415c6fef99d91bf65343
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2025-03-26 20:29:15 +0530

    type annotaions

---
+++ l1 type annotations/index.ts
@@ -0,0 +1,17 @@
+//type Annotations -> used for type safety
+
+//string
+let userName : String="dhaya";
+console.log(userName);
+
+//number
+let age : number=21;
+console.log(age);
+
+//boolean
+//normal
+//let isAdult : boolean=true;
+
+//to check conditionly
+let isAdult : boolean=age>28;
+console.log(isAdult);
